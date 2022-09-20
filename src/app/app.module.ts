964adaa89fc34ec1ff1c95fa9dd7d5a474867ffa
preethi <68074514+preethidaran@users.noreply.github.com>
2022-09-20 17:13:59 +0530

    angular material add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { MatTableModule } from '@angular/material/table';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { UserFormComponent } from './user-form/user-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TableComponent,
-    UserFormComponent
-  ],
+  declarations: [AppComponent, TableComponent, UserFormComponent],
   imports: [
     BrowserModule,
+    AngularMaterialModule,
     AppRoutingModule,
     MatTableModule,
     MatDialogModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents : [UserFormComponent]
+  entryComponents: [UserFormComponent],
 })
-export class AppModule { }
+export class AppModule {}
